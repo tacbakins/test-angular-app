e43f738c942b609b61421556eb91edcb7e2fea3c
Brandon <bakins@toyotomiam.com>
2020-06-24 15:07:17 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <body id="something" class="some-class"> 'body', '#something', '.some-class'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-asp-app';
+  title = 'Test app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    PropertyCardComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule
